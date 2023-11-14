88f6a40ff34557b726c63e672149b7f99d9512ba
J-Ivan-MC <josue.macedo.60096@usalesiana.edu.bo>
2023-11-14 15:33:39 -0400

    [@J-Ivan-MC] include product card component

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Grid  from '@mui/material/Grid';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { products, addToCart, minusItem, plusItem, getTotal } from '../../apis/service';
 
 function ProductCard({ product }) {
-  const { title, image, description, price, stock } = product;
+  const { id , title, image, description, price } = product;
+
+  const handleAddToCart = () => {
+    addToCart(product);
+  };
+
 
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
@@ -27,43 +32,14 @@ function ProductCard({ product }) {
             {description}
           </Typography>
           <Typography variant="body1" color="primary">
-            Precio: ${price}
+            Price: ${price}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Stock disponible: {stock}
-          </Typography>
-          <Button variant="contained" color="primary">
-            Agregar al carrito
+          <Button variant="contained" color="primary"  onClick={handleAddToCart}>
+            addToCard
           </Button>
         </CardContent>
       </Card>
     </Grid>
-  );
-}
-
-export function ProductCard2() {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="	https://mui.com/static/images/cards/contemplative-reptile.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
   );
 }
 
